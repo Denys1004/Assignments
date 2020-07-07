d498fa6d65c20a57d68faafe727931c6e8947eeb
denys krychevskyi <krychevskyidenys@gmail.com>
2020-07-07 16:46:12 -0700

    done with semirestfull

---
+++ semi_restful_tv_shows/app_tv_shows/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('shows', views.all_shows),
+    path('shows/new', views.add_new_show),
+    path('process_new_show', views.process_new_show),
+    path('shows/<int:id>', views.show_one_tvshow),
+    path('edit/<int:id>', views.edit),
+    path('update', views.update),
+
+    path('delete/<int:id>', views.delete)
+
+]
--- semi_restful_tv_shows/app_tv_shows/views.py
+++ semi_restful_tv_shows/app_tv_shows/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
+def index(request):
+    return redirect('/shows')
+
+def all_shows(request):
+    context = {
+        'all_shows': Show.objects.all()
+    }
+    return render(request, 'all_shows.html', context)
+
+def add_new_show(request):
+    return render(request, 'add_new_show.html')
+
+def process_new_show(request):
+    new_show = Show.objects.create(
+        title = request.POST['title'],
+        network = request.POST['network'],
+        release_date = request.POST['release_date'],
+        description = request.POST['description']
+        )
+    return redirect('/shows')
+
+def show_one_tvshow(request, id):
+    num_id = int(id)
+    context = {
+        'needed_tv_show':Show.objects.get(id = num_id)
+    } 
+    return render(request, 'tv_show.html', context)
+
+def edit(request, id):
+    num_id = int(id)
+    context = {
+        'needed_tv_show':Show.objects.get(id = num_id)
+    } 
+    return render(request, 'edit.html', context) 
+
+def update(request):
+    num_id = int(request.POST['id'])
+    str_id = request.POST['id']
+    edited_show = Show.objects.get(id = num_id)
+    edited_show.title = request.POST['title']
+    edited_show.network = request.POST['network']
+    edited_show.release_date = request.POST['release_date']
+    edited_show.description = request.POST['description']
+    edited_show.save()
+    return redirect('/shows')
+
+
+def delete(request, id):
+    num_id = int(id)
+    needed_show = Show.objects.get(id = num_id)
+    needed_show.delete()
+
+    return redirect('/shows') 
