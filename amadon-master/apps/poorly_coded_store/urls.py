3bf85d1c1301a5cee15be0af4bd117193e5b5ee4
denys krychevskyi <krychevskyidenys@gmail.com>
2020-07-11 08:57:15 -0700

    some

--- amadon-master/apps/poorly_coded_store/urls.py
+++ amadon-master/apps/poorly_coded_store/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url('', views.index),
-    url('process', views.process),
-    url('checkout', views.checkout)
+    path('', views.index),
+    path('checkout', views.checkout),
+    path('process', views.process),
 ]
--- amadon-master/apps/poorly_coded_store/views.py
+++ amadon-master/apps/poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 from django.db.models import Sum
 
